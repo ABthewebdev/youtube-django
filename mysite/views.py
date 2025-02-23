ce9e65fca0e6f6cdc7024a7ebafddd38812c8248
alexml <alexgbrewster@gmail.com>
2025-02-23 12:40:53 -0500

    create list

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import CreateNewList
 
 # Create your views here.
+def home(response):
+    return render(response, 'mysite/home.html', {})
+
+def create(response):
+    return render(response, 'mysite/list.html', {})
